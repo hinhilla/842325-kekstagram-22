d2c0e00878c4c644c1e940c3ed9d4ab1b8fff0bd
Kitkat <hinhilla@list.ru>
2021-06-08 17:17:04 +0300

    Сокращение в ES6

--- js/api.js
+++ js/api.js
@@ -5,22 +5,29 @@ import { getBigPictureInfo } from './show-full.js';
 const URL = 'https://22.javascript.pages.academy/kekstagram/data'
 
 fetch(URL)
-  .then((response) => {
-    return toJSON(response)
-  })
+  .then((response) => toJSON(response))
   .then((photos) => {
     addCardOnPage(photos);
     getBigPictureInfo(photos);
-    console.log(photos);
+    // console.log(photos);
+
   })
-  .catch(() => {
-    console.log('ошибка загрузки данных');
+  .catch((error) => {
+    console.log(error);
   })
 
 const toJSON = (response) => {
   if (response.ok) {
+
+
     return response.json()
   } else {
     throw Error;
   }
 }
+
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-import './fetch.js'
+import './api.js'
 import './show.js';
 import './show-full.js'
 import './upload-form.js'
 import './scale.js'
 import './form-validity.js'
+
+
 
 
