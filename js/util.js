0f2eb860cb0dac87a740ab92bb8a78e056c9070b
Kitkat <hinhilla@list.ru>
2021-06-10 14:44:06 +0300

    Фильтрация изображений с устранением дребезга

--- js/util.js
+++ js/util.js
@@ -7,11 +7,11 @@
 //   }
 // };
 
-// const INITIAL_COUNT = 25;
+const INITIAL_COUNT = 25;
 // const generateArayWithInfo = (length, cb) => new Array(length).fill().map(cb);
 // const getRandomElement = (array) => array[getRandomNumber(0, array.length - 1)];
 // const getRandomId = () => Math.floor(Math.random() * Math.floor(Math.random() * Date.now()));
-// const generateUnicNumbers = (from = 1, to = INITIAL_COUNT, n = to) => [...Array(to - from + 1).keys()].map(i => i + from).reduce((arr, el) => (arr.splice(Math.random() * (arr.length + 1), 0, el), arr), []).slice(0, n);
+const mixNumbersFromTo = (from = 0, to = INITIAL_COUNT - 1, n = to - from + 1) => [...Array(to - from + 1).keys()].map(i => i + from).reduce((arr, el) => (arr.splice(Math.random() * (arr.length + 1), 0, el), arr), []).slice(0, n);
 
 
 
@@ -32,5 +32,5 @@ const closeModal = (element, body, hidden, open) => {
 // export { getRandomElement };
 // export { getRandomId };
 // export { generateUnicNumbers };
-export { openModal, closeModal };
+export { openModal, closeModal, mixNumbersFromTo, INITIAL_COUNT };
 // export { closeModal };
